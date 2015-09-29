7c3c9c5cbd94bc2cdba127c634d0b6171d3a367f
ceciliachalar <ceci.chiqui@gmail.com>
2015-09-29 17:10:33 -0400

    Jquery form

--- jquery-exercise/SampleFormTest.js
+++ jquery-exercise/SampleFormTest.js
@@ -3,12 +3,34 @@
  */
 myFormTest = new SampleFormTest();
 myFormTest.validateAllFieldsAreRequired();
-
-
-
 this.validateAllFieldsAreRequired= function(){
 
+    var setFullName = function(){
+        $('input[name="fullname"]').val('Ceci Test');
+    };
 
+    var setEmail = function(){
+        $('input[name="email"]').val('email@dominio.com');
+    };
+    var Gender = function(){
+        $('input[name="genderF"]').attr('checked', true);
+    };
 
+    var setHobbies = function(){
+        //select skiing and running
+        $('input#hobby1').attr('checked', true);
+        $('input#hobby2').attr('checked', true);
+    };
 
+    var chooseHeard = function(){
+        $('select#heard').val('mouth');
+    };
+
+    var writeMessage = function(){
+        $('textarea#message').text('message writed');
+    };
+
+    var clickButton = function(){
+        $('span.btn.btn-default').click();
+    };
 };
